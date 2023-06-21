79fe8a77105f50eae2d3000f680d75642857eb98
Khang Nguyễn <khang.nguyen@realstake.io>
2023-06-21 08:00:34 +0700

    feat(user): using user query for testing

---
+++ libs/shared/src/types/result.ts
@@ -0,0 +1,6 @@
+import { BaseException } from '@lib/shared';
+import { Result } from 'oxide.ts';
+
+export { Err, Ok, Result } from 'oxide.ts';
+
+export type Either<T = unknown> = Result<T, BaseException>;
