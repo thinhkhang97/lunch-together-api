dcff8a107e25c433e9336c93cd29a0547d6f2bdc
Khang Nguyễn <khang.nguyen@realstake.io>
2023-06-27 17:13:11 +0700

    init(app): init base command

---
+++ libs/shared/src/ddd/applications/commands/index.ts
@@ -0,0 +1,2 @@
+export * from './base.command';
+export * from './base.command-handler';
--- libs/shared/src/ddd/applications/queries/base.query-handler.ts
+++ libs/shared/src/ddd/applications/queries/base.query-handler.ts
@@ -1,6 +1,11 @@
-import { BaseException, BaseQuery, InternalServerException } from '@lib/shared';
+import {
+  BaseException,
+  BaseQuery,
+  InternalServerException,
+  Result,
+} from '@lib/shared';
 import { IQueryHandler } from '@nestjs/cqrs';
-import { Err, Ok, Result } from 'oxide.ts';
+import { Err, Ok } from 'oxide.ts';
 
 export abstract class BaseQueryHandler<Query extends BaseQuery, Response>
   implements IQueryHandler
